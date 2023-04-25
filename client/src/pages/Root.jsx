12e8016b54eabc763edf892a753beb5ae416ce51
DawidZ <dawid.ziobrowski@wp.pl>
2023-04-25 12:00:36 +0200

    Root page styling

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -21,23 +21,32 @@ function Root() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Chat application</p>
-        <div>
-          <span>Enter your nick</span>
-          <input type="text" ref={nickName}/>
+    <nav>
+      <div class="nav-wrapper blue">
+        <a href="#" class="brand-logo center">Super chat</a>
+      </div>
+    </nav>
+      <div className="container app-container">
+        <div className="row">
+          <div class="input-field col s6">
+            <input id="nickname" type="text" class="validate" ref={nickName} />
+            <label for="nickname">Enter your nickname</label>
+          </div>
         </div>
-        <div>
-          <span>Room number</span>
-          <input type="number" ref={room}/>
-          <button
-            onClick={joinRoom}
-          >
-            Join room
-          </button>
+        <div className="row">
+          <div class="input-field col s6">
+            <input id="room" type="number" class="validate" ref={room} />
+            <label for="room">Enter room number</label>
+          </div>
+        </div>
+        <div className="row">
+          <a onClick={joinRoom} class="waves-effect waves-light btn blue">
+            <i class="material-icons left">group_add</i>
+            Join channel
+          </a>
+        </div>
           
-        </div>
-      </header>
+      </div>
     </div>
   );
 }
